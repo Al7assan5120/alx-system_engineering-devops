4cedbd917f0f0212805efd08f8d4c31ceefaaf82
Al7assan <al7assan.r@gmail.com>
2024-01-19 22:42:08 +0200

    c 1.4

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
+#!/usr/bin/pup
 # Installs flask, version 2.1.0
+
+package { 'python3-pip':
+  ensure => Installs,
+}
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require => package['python3-pip'],
+}
+
+package { 'werkeug':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require => package['python3-pip'],
 }
