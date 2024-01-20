8abc2500ce259f71cdb8173484233778bce5b29d
Al7assan <al7assan.r@gmail.com>
2024-01-20 18:39:45 +0200

    c 100.1

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,12 +2,12 @@
 #use puppet to make changes to my config files
 #this script is to be run on the puppet master
 file { '/etc/ssh/ssh_config':
-    ensure => present,
-    content => "
-    Include /etc/ssh/ssh_config.d/*.conf
-    Host *
+        ensure => present,
+content => "
+Include /etc/ssh/ssh_config.d/*.conf
+Host *
     IdentityFile ~/.ssh/school
     passwordAuthentication no
-    ",
+",
   mode    => '0644',
 }
